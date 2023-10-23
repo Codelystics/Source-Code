71b4394f174957b2b1b61d937c1a6cc76f106b69
Michael Jonathan <michael.jonathan004@binus.ac.id>
2023-10-24 04:47:22 +0700

    Homepage 90%, Bootcamp 20%

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <div class="absolute top-36 -left-16 -z-10">
+    <div class="absolute bottom-20 -left-16 -z-10">
         <img src="{{ asset('shapes1.svg') }}" alt="">
     </div>
     <div class="flex flex-row px-32 h-full">
@@ -24,13 +24,13 @@
         <div class="text-white text-5xl pb-10">
             SCHEDULE OF THIS MONTH
         </div>
-        <div class="flex flex-row">
+        <div class="flex flex-row w-full">
             <div class="flex flex-row">
                 <img src="{{ asset('secondImage.svg') }}" alt="">
             </div>
-            <div class="flex flex-col justify-evenly items-end w-1/2">
-                <div class="flex flex-row justify-around items-center h-32 w-4/5 bg-black bg-opacity-0 border-l-4 border-violet hover:bg-opacity-25 transition ease-in-out duration-300 hover:border-l-4 hover:border-yellow">
-                    <div class="flex flex-col justify-center items-center text-white w-1/4 h-full text-4xl">
+            <div class="flex flex-col justify-evenly items-end w-2/3 gap-7">
+                <div class="flex flex-row justify-around items-center h-full w-4/5 bg-black bg-opacity-0 border-l-4 border-violet hover:bg-opacity-25 transition ease-in-out duration-300 hover:border-l-4 hover:border-yellow">
+                    <div class="flex flex-col justify-center items-center text-white w-1/4 h-full text-3xl">
                         <div>
                             15
                         </div>
@@ -39,16 +39,16 @@
                         </div>
                     </div>
                     <div class="flex flex-col justify-center items-start w-3/4 h-full">
-                        <div class="text-white text-opacity-100 text-3xl">
+                        <div class="text-white text-opacity-100 text-2xl">
                             Yapping Programming Language
                         </div>
-                        <div class="text-white text-opacity-80 text-2xl">
+                        <div class="text-white text-opacity-80 text-xl">
                             Semanggi, Senayan City
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-row justify-around items-center h-32 w-4/5 bg-black bg-opacity-0 border-l-4 border-violet hover:bg-opacity-25 transition ease-in-out duration-300 hover:border-l-4 hover:border-yellow">
-                    <div class="flex flex-col justify-center items-center text-white w-1/4 h-full text-4xl">
+                <div class="flex flex-row justify-around items-center h-full w-4/5 bg-black bg-opacity-0 border-l-4 border-violet hover:bg-opacity-25 transition ease-in-out duration-300 hover:border-l-4 hover:border-yellow">
+                    <div class="flex flex-col justify-center items-center text-white w-1/4 h-full text-3xl">
                         <div>
                             20
                         </div>
@@ -57,16 +57,16 @@
                         </div>
                     </div>
                     <div class="flex flex-col justify-center items-start w-3/4 h-full">
-                        <div class="text-white text-opacity-100 text-3xl">
+                        <div class="text-white text-opacity-100 text-2xl">
                             PHP Newbies Talk
                         </div>
-                        <div class="text-white text-opacity-80 text-2xl">
+                        <div class="text-white text-opacity-80 text-xl">
                             Palmerah, Kemanggisan
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-row justify-around items-center h-32 w-4/5 bg-black bg-opacity-0 border-l-4 border-violet hover:bg-opacity-25 transition ease-in-out duration-300 hover:border-l-4 hover:border-yellow">
-                    <div class="flex flex-col justify-center items-center text-white w-1/4 h-full text-4xl">
+                <div class="flex flex-row justify-around items-center h-full w-4/5 bg-black bg-opacity-0 border-l-4 border-violet hover:bg-opacity-25 transition ease-in-out duration-300 hover:border-l-4 hover:border-yellow">
+                    <div class="flex flex-col justify-center items-center text-white w-1/4 h-full text-3xl">
                         <div>
                             16
                         </div>
@@ -75,10 +75,10 @@
                         </div>
                     </div>
                     <div class="flex flex-col justify-center items-start w-3/4 h-full">
-                        <div class="text-white text-opacity-100 text-3xl">
+                        <div class="text-white text-opacity-100 text-2xl">
                             It’s Golang, not Go
                         </div>
-                        <div class="text-white text-opacity-80 text-2xl">
+                        <div class="text-white text-opacity-80 text-xl">
                             Sudirman, FX Mall
                         </div>
                     </div>
@@ -103,14 +103,14 @@
             <p class="text-4xl text-white text-opacity-80 font-extrabold">Events in our site from 2018 - 2023</p>
         </div>
     </div>
-    <div class="pt-32 w-full h-full px-20">
-        <div class="flex flex-row w-full h-full justify-between items-center text-white">
-            <div class="w-1/3 flex flex-col justify-between items-start bg-plum mx-6 p-20 rounded-md">
+    <div class="pt-32 w-full h-full">
+        <div class="flex flex-row w-full h-full justify-evenly items-center text-white gap-6">
+            <div class="flex flex-col justify-between items-start bg-plum px-12 pb-10 pt-20 rounded-md">
                 <div class="flex flex-row text-4xl font-extrabold pb-8">
                     Virtual Conference
                 </div>
                 <div class="flex flex-row items-end gap-4">
-                    <div class="text-8xl font-extrabold">
+                    <div class="text-7xl font-extrabold">
                         650
                     </div>
                     <div class="text-3xl">
@@ -118,12 +118,12 @@
                     </div>
                 </div>
             </div>
-            <div class="w-1/3 flex flex-col justify-center items-start bg-violet mx-6 p-20 rounded-md">
+            <div class="flex flex-col justify-center items-start bg-violet px-12 pb-10 pt-20 rounded-md">
                 <div class="flex flex-row text-4xl font-extrabold pb-8">
                     Virtual Conference
                 </div>
                 <div class="flex flex-row items-end gap-4">
-                    <div class="text-8xl font-">
+                    <div class="text-7xl font-">
                         650
                     </div>
                     <div class="text-3xl">
@@ -131,12 +131,12 @@
                     </div>
                 </div>
             </div>
-            <div class="w-1/3 flex flex-col justify-center items-start bg-lavender mx-6 p-20 rounded-md">
+            <div class="flex flex-col justify-center items-start bg-lavender px-12 pb-10 pt-20 rounded-md">
                 <div class="flex flex-row text-4xl font-extrabold pb-8">
                     Virtual Conference
                 </div>
                 <div class="flex flex-row items-end gap-4">
-                    <div class="text-8xl font-extrabold">
+                    <div class="text-7xl font-extrabold">
                         650
                     </div>
                     <div class="text-3xl">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Home/index');
 });
+
+Route::get('/bootcamp', function () {
+    return view('Bootcamp/index');
+});
+
+Route::get('/competition', function () {
+    return view('Competition/index');
+});
