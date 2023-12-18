8691e56cd6542d1d68df51eda831207737a77c65
erio_y <erioyoshino99@gmail.com>
2023-12-18 21:07:33 +0700

    login kelar
    seeder juga tak ganti, jadi dia ga 100% gacha

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,70 +19,70 @@ class UserSeeder extends Seeder
             [
                 'username' => 'admin',
                 'email' => 'admin@example.com',
-                'password' => 'admin',
+                'password' => Hash::make('admin'), 
                 'is_admin' => 1,
                 'is_verified' => 0
             ],
             [
                 'username' => 'Mikel Jon tan',
                 'email' => 'mikel@example.com',
-                'password' => 'mikel',
+                'password' => Hash::make('mikel'),
                 'is_admin' => 0,
                 'is_verified' => 1
             ],
             [
                 'username' => 'Jefri Dam',
                 'email' => 'jeff@example.com',
-                'password' => 'jeff',
+                'password' => Hash::make('jeff'),
                 'is_admin' => 0,
                 'is_verified' => 1
             ],
             [
                 'username' => 'Steven Handoko',
                 'email' => 'steven@example.com',
-                'password' => 'steven',
+                'password' => Hash::make('steven'),
                 'is_admin' => 0,
                 'is_verified' => 1
             ],
             [
                 'username' => 'Err Yoshi',
                 'email' => 'err@example.com',
-                'password' => 'err',
+                'password' => Hash::make('err'),
                 'is_admin' => 0,
                 'is_verified' => 1
             ],
             [
                 'username' => 'Juna Chrish',
                 'email' => 'juan@example.com',
-                'password' => 'juan',
+                'password' => Hash::make('juan'),
                 'is_admin' => 0,
                 'is_verified' => 0
             ],
             [
                 'username' => 'Mas Hansen',
                 'email' => 'hansen@example.com',
-                'password' => 'hansen',
+                'password' => Hash::make('hansen'),
                 'is_admin' => 0,
                 'is_verified' => 0
             ],
             [
                 'username' => 'Julen Hitam',
                 'email' => 'julen@example.com',
-                'password' => 'julen',
+                'password' => Hash::make('julen'),
                 'is_admin' => 0,
                 'is_verified' => 0
             ],
             [
                 'username' => 'Reza Kecap',
                 'email' => 'reza@example.com',
-                'password' => 'reza',
+                'password' => Hash::make('reza'),
                 'is_admin' => 0,
                 'is_verified' => 0
             ],
             [
                 'username' => 'dummy',
                 'email' => 'dummy@example.com',
-                'password' => 'dummy',
+                'password' => Hash::make('dummy'),
                 'is_admin' => 0,
                 'is_verified' => 0
             ],
